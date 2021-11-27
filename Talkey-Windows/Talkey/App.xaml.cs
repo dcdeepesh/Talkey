33b4e81fee0b1c6b8c325df7cd8631b69e1e39d6
Deepesh Choudhary <dcdeepesh7@gmail.com>
2021-11-27 18:58:48 +0530

    Add tray icon

--- Talkey-Windows/Talkey/App.xaml.cs
+++ Talkey-Windows/Talkey/App.xaml.cs
@@ -2,16 +2,36 @@
 
 using Handlers;
 
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
+using ContextMenu = System.Windows.Forms.ContextMenu;
+using MenuItem = System.Windows.Forms.MenuItem;
+
 namespace Talkey {
     public partial class App : Application {
+        NotifyIcon trayIcon;
+
         public App() {
             HookHandler.Init();
             IPCHandler.Init();
+            InitTrayIcon();
         }
 
         private void OnExit(object sender, ExitEventArgs e) {
             HookHandler.Shutdown();
             IPCHandler.Shutdown();
+            trayIcon.Dispose();
+        }
+
+        private void InitTrayIcon() {
+            MenuItem menuItem = new MenuItem("Exit", (sender, e) => Application.Current.Shutdown());
+            ContextMenu menu = new ContextMenu(new[] { menuItem });
+
+            trayIcon = new NotifyIcon {
+                Icon = Talkey.Properties.Resources.TrayIcon,
+                Text = "Talkey (Google Meet Push-to-talk)",
+                ContextMenu = menu,
+                Visible = true
+            };
         }
     }
 }
