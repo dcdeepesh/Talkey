f4360499c9c70af04fab49388becd88dc7fbcae4
Deepesh Choudhary <dcdeepesh7@gmail.com>
2021-11-28 17:47:15 +0530

    Set url condition and fix icons

--- Talkey-Chrome/background.ts
+++ Talkey-Chrome/background.ts
@@ -4,50 +4,30 @@ let state = State.Off;
 let socket: WebSocket;
 let tabId: number;
 
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.browserAction.setIcon({
-        path: {
-            "16": "icons/off/icon16.png",
-            "32": "icons/off/icon32.png",
-            "48": "icons/off/icon48.png",
-            "128": "icons/off/icon128.png",
-            "256": "icons/off/icon256.png"
-        }
-    });
-});
+chrome.runtime.onInstalled.addListener(() => setIcon('off'));
 
 chrome.browserAction.onClicked.addListener(() => {
     if (state == State.Off) {
         chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            let id = tabs[0].id;
-            if (id === undefined)
-                0; // TODO error
-            else tabId = id;
-            console.log(tabId);
-        });
-        chrome.browserAction.setIcon({
-            path: {
-                "16": "icons/conn/icon16.png",
-                "32": "icons/conn/icon32.png",
-                "48": "icons/conn/icon48.png",
-                "128": "icons/conn/icon128.png",
-                "256": "icons/conn/icon256.png"
+            let tab = tabs[0];
+            if (tab.url === undefined || tab.id === undefined)
+                return; // TODO error
+            
+            if (!/^https?:\/\/meet\.google\.com\//i.test(tab.url)) {
+                setIcon('error');
+                setTimeout(() => setIcon('off'), 500);
+                return;
             }
+            
+            tabId = tab.id;
+            setIcon('conn');
+            state = State.Connecting;
+            initWebsocket();
         });
-        state = State.Connecting;
-        initWebsocket();
     } else {
         try { closeWebsocket(); }
         catch (e) {}
-        chrome.browserAction.setIcon({
-            path: {
-                "16": "icons/off/icon16.png",
-                "32": "icons/off/icon32.png",
-                "48": "icons/off/icon48.png",
-                "128": "icons/off/icon128.png",
-                "256": "icons/off/icon256.png"
-            }
-        });
+        setIcon('off');
         state = State.Off;
     }
 });
@@ -55,15 +35,7 @@ chrome.browserAction.onClicked.addListener(() => {
 function initWebsocket() {
     socket = new WebSocket(`ws://localhost:${WS_PORT}/`)
     socket.addEventListener('open', () => {
-        chrome.browserAction.setIcon({
-            path: {
-                "16": "icons/on/icon16.png",
-                "32": "icons/on/icon32.png",
-                "48": "icons/on/icon48.png",
-                "128": "icons/on/icon128.png",
-                "256": "icons/on/icon256.png"
-            }
-        });
+        setIcon('on');
         state = State.On;
     });             
     socket.addEventListener('message', onWSMessage);
@@ -83,10 +55,19 @@ function onWSMessage(event: MessageEvent<any>) {
 
 function mute() {
     chrome.tabs.sendMessage(tabId, { "action": "mute" });
-    chrome.browserAction.setBadgeText({text: "M"});
 }
 
 function unmute() {
     chrome.tabs.sendMessage(tabId, { "action": "unmute" });
-    chrome.browserAction.setBadgeText({text: "UM"});
+}
+
+function setIcon(iconType: String) {
+    chrome.browserAction.setIcon({
+        path: {
+            "16": `icons/${iconType}/icon16.png`,
+            "32": `icons/${iconType}/icon32.png`,
+            "48": `icons/${iconType}/icon48.png`,
+            "128": `icons/${iconType}/icon128.png`,
+        }
+    });
 }
