aa20ce8a573289269f3dfe3bf358ba5521b77d48
Deepesh Choudhary <dcdeepesh7@gmail.com>
2021-11-29 00:36:00 +0530

    Add key preview in tray window

---
+++ Talkey-Windows/Talkey/Key.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace Talkey {
+    public partial class Key : UserControl {
+        public Key() {
+            InitializeComponent();
+        }
+    }
+}
--- Talkey-Windows/Talkey/TrayWindow.xaml.cs
+++ Talkey-Windows/Talkey/TrayWindow.xaml.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
+using Handlers;
 
 namespace Talkey {
-    /// <summary>
-    /// Interaction logic for TrayWindow.xaml
-    /// </summary>
     public partial class TrayWindow : Window {
         public TrayWindow() {
             InitializeComponent();
+
+            foreach (VK key in KeyHandler.CurrentKeyCombo) {
+                Key keyView = new Key();
+                keyView.KeyName.Text = key.ToString();
+                Keys.Children.Add(keyView);
+            };
         }
     }
 }
